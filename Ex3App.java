9243250b69d4532310ae1be5cadebd76b58faab7
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-15 10:13:27 +0200

    Create Ex3App.java

---
+++ Ex3App.java
@@ -0,0 +1,26 @@
+
+public class Ex3App {
+
+	public static void masPaginas (Libro libro1,Libro libro2) {
+		
+		if(libro1.numero_paginas<libro2.numero_paginas) {
+			System.out.println("Libro 2 tiene mas paginas");
+		}else {
+			System.out.println("Libro 1 tiene mas paginas");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Libro libro1 = new Libro(1234,"Libro 1","Autor 1",24);
+		Libro libro2 = new Libro(4321,"Libro 2","Autor 2",30);
+		
+		System.out.println(libro1.toString());
+		System.out.println(libro2.toString());
+		
+		masPaginas(libro1,libro2);
+		
+	}
+	
+}
